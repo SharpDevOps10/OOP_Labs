cee01dd80edf39afda0cae0f866a6980e2d12bb3
SharpDevOps10 <td090833@gmail.com>
2023-10-09 20:38:02 +0300

    Updated `LineShape` class

--- Lab2/app/src/main/java/com/example/lab2/shape_primitives/LineShape.kt
+++ Lab2/app/src/main/java/com/example/lab2/shape_primitives/LineShape.kt
@@ -2,20 +2,33 @@ package com.example.lab2.shape_primitives
 
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.DashPathEffect
 import android.graphics.Paint
 import com.example.lab2.Shape
 
-class LineShape (private val paintSettings: Paint) : Shape(paintSettings) {
+class LineShape(paintSettings: Paint) : Shape(paintSettings) {
+  private var isDashed = true
+  private val paint = Paint()
+
+  fun toggleDashed () {
+    isDashed = !isDashed
+  }
+
   override fun draw (canvas: Canvas) {
     configureDrawing()
-    canvas.drawLine(startXCoordinate, startYCoordinate, endXCoordinate, endYCoordinate, paintSettings)
+    if (isDashed) {
+      paint.pathEffect = DashPathEffect(floatArrayOf(10f, 10f), 0f)
+    } else {
+      paint.pathEffect = null
+    }
+    canvas.drawLine(startXCoordinate, startYCoordinate, endXCoordinate, endYCoordinate, paint)
   }
 
   override fun configureDrawing () {
-    paintSettings.apply {
+    paint.apply {
       color = Color.BLACK
       style = Paint.Style.FILL_AND_STROKE
+      strokeWidth = 20f
     }
   }
-
 }
