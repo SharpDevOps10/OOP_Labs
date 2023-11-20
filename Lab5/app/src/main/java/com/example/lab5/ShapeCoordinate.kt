e93f6d3db437dae79657257bc63f85fbf4bff6e3
SharpDevOps10 <td090833@gmail.com>
2023-11-20 22:11:23 +0200

    enhancement #60: update Shape.kt
    closes #60

---
+++ Lab5/app/src/main/java/com/example/lab5/ShapeCoordinate.kt
@@ -0,0 +1,10 @@
+package com.example.lab5
+
+data class ShapeCoordinate (
+  val shapeType: String,
+  val startX: String,
+  val startY: String,
+  val endX: String,
+  val endY: String,
+  val shapeReference: Shape
+)
